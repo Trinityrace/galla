3b0323d7fedaed114e869d6437da159a991e59f5
Trinity John <trinityrtime@gmail.com>
2020-05-25 23:00:47 +0300

    models input for class photos

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+  title = models.CharField(max_length=200)
+  width = models.IntegerField(default=0)
+  height = models.IntegerField(default=0)
+  image = models.ImageField(null=False, blank=False, width_field="width", height_field="height")
+  timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
