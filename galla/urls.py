8e5f5529deb2f0a6f71cce83e95f4be3e4321ada
Trinity John <trinityrtime@gmail.com>
2020-05-26 02:22:53 +0300

    bug fix for images not showing uploaded by admin

--- galla/urls.py
+++ galla/urls.py
@@ -4,11 +4,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from gallery.views import index
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('gallery.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]
+urlpatterns += staticfiles_urlpatterns()
 
 # if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -4,5 +4,5 @@ from django.http  import HttpResponse, Http404
 
 # Create your views here.
 def index(request):
-    images = Photo.objects.all()
+    images = Photo.objects.all().order_by('timestamp')
     return render(request, 'photo/photo.html', {'images': images})
