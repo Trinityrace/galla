43b7f39bb0f9a1b1af1b5c97e3739b4e3bc09575
Trinity John <trinityrtime@gmail.com>
2020-05-25 22:45:50 +0300

    media file and route added in setting. urls for gallery app

--- galla/settings.py
+++ galla/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media")
