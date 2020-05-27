e871d2d11d8c9594d204a5971cd32dcc972d2b56
Trinity John <trinityrtime@gmail.com>
2020-05-27 20:07:00 +0300

    migrations value error fix for default location and category

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,48 @@ import datetime as dt
 
 # Create your models here.
 Default_description = "Photographical Art At Its Best!"
-# photo model
+default_location = "port"
+default_category = "art"
+
+# location class and methods
+class Location(models.Model):
+  place = models.CharField(max_length=30)
+
+  def __unicode__(self):
+      return self.place
+
+  @classmethod
+  def save_location(self):
+      self.save()
+  
+  @classmethod 
+  def delete_location(self):
+      self.delete()
+      
+  @classmethod
+  def update_location(cls, id, new_location):
+      cls.objects.filter(id=id).update(Location=new_location) 
+
+# category class and methods
+class Category(models.Model):
+  Name = models.CharField(max_length=30)
+
+  def __unicode__(self):
+    return self.Name
+
+  @classmethod
+  def save_category(self):
+      self.save()
+      
+  @classmethod
+  def delete_category(self):
+      self.delete()
+      
+  @classmethod
+  def update_category(cls, id, new_category):
+      cls.objects.filter(id=id).update(Category=new_category)
+
+# photo class model
 class Photo(models.Model):
   title = models.CharField(max_length=70)
   width = models.IntegerField(default=0)
@@ -45,45 +86,5 @@ class Photo(models.Model):
 
   @classmethod
   def search_by_location(cls, search_term):
-      photo = Photo.objects.filter(location__id=search_term).all()
-      return photo
-
-
-# location class and methods
-class Location():
-  place = models.CharField(max_length=30)
-
-  def __str__(self):
-      return self.place
-
-  @classmethod
-  def save_location(self):
-      self.save()
-  
-  @classmethod 
-  def delete_location(self):
-      self.delete()
-      
-  @classmethod
-  def update_location(cls, id, new_location):
-      cls.objects.filter(id=id).update(location=new_location) 
-
-# category class and methods
-class Category(models.Model):
-  Name = models.CharField(max_length=30)
-
-  def __str__(self):
-    return self.Name
-
-  @classmethod
-  def save_category(self):
-      self.save()
-      
-  @classmethod
-  def delete_category(self):
-      self.delete()
-      
-  @classmethod
-  def update_category(cls, id, new_category):
-      cls.objects.filter(id=id).update(Category=new_category)
-
+      image = Photo.objects.filter(location__id=search_term).all()
+      return image
