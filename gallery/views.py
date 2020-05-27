ecaae2fbbe7066a21d022df3bfd8b764158c96d6
Trinity John <trinityrtime@gmail.com>
2020-05-27 16:49:18 +0300

    created html tempalte files for different functions

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Photo
 from django.http  import HttpResponse, Http404
+import datetime as dt
 
 # Create your views here.
 def index(request):
     images = Photo.objects.all().order_by('timestamp')
-    return render(request, 'photo/photo.html', {'images': images})
+    return render(request, 'photo/base.html', {'images': images})
+
+
+def location():
+    return render(request)
+
+
+def category():
+    return render(request, 'photo/category.html')
+
+def search_image_by_id(id):
+    return render(request, 'photo/search.html')
