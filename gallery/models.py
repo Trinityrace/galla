1496f7d161d760063f3c0de1ec6b57058f656a38
Trinity John <trinityrtime@gmail.com>
2020-05-27 20:55:56 +0300

    models fix for images removed deafult jpg

--- gallery/models.py
+++ gallery/models.py
@@ -50,7 +50,7 @@ class Photo(models.Model):
   title = models.CharField(max_length=70)
   width = models.IntegerField(default=0)
   height = models.IntegerField(default=0)
-  image = models.ImageField(null=False, blank=False, width_field="width", height_field="height", default='default.jpg')
+  image = models.ImageField(null=False, blank=False, width_field="width", height_field="height")
   timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
   descripion = models.TextField(default=Default_description)
   location = models.ForeignKey(Location, on_delete=models.CASCADE)
