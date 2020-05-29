ef32717d7982c9455044b0b13965166348b65bd4
Trinity John <trinityrtime@gmail.com>
2020-05-29 12:27:28 +0300

    test cases for location and category models

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+# test for categories 
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.art = category(Name='art')
+
+    def test_instance(self):
+        self.art.save()
+        self.assertTrue(isinstance(self.art, category))
+
+# test for location
+class LocationTest(TestCase):
+    def setUp(self):
+        self.dubai = location(place='dubai')
+
+    def test_instance(self):
+        self.dubai.save()
+        self.assertTrue(isinstance(self.dubai, location))
+
