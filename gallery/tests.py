db154e4da2f036f46165e5fb9858c88b2987e7bf
Trinity John <trinityrtime@gmail.com>
2020-05-29 12:32:18 +0300

    2 tests created passed for both location and ctaegory

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,21 +1,22 @@
 from django.test import TestCase
+from .models import Location, Category, Photo
 
 # Create your tests here.
 # test for categories 
 class CategoryTest(TestCase):
     def setUp(self):
-        self.art = category(Name='art')
+        self.art = Category(Name='art')
 
     def test_instance(self):
         self.art.save()
-        self.assertTrue(isinstance(self.art, category))
+        self.assertTrue(isinstance(self.art, Category))
 
 # test for location
 class LocationTest(TestCase):
     def setUp(self):
-        self.dubai = location(place='dubai')
+        self.dubai = Location(place='dubai')
 
     def test_instance(self):
         self.dubai.save()
-        self.assertTrue(isinstance(self.dubai, location))
+        self.assertTrue(isinstance(self.dubai, Location))
 
